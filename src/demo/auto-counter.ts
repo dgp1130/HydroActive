232614e8b4fe0cddd25f796ade2bbb8d2a20c2bd
Doug Parker <dgp1130@users.noreply.github.com>
2023-12-15 22:30:46 -0800

    Updates `auto-counter` to use signals.

--- src/demo/auto-counter.ts
+++ src/demo/auto-counter.ts
@@ -1,18 +1,22 @@
 import { component } from 'hydroactive';
+import { signal } from 'hydroactive/signals.js';
 
 /** Automatically increments the count over time. */
 export const AutoCounter = component('auto-counter', (comp) => {
   const label = comp.host.query('span')!;
-  let count = Number(label.text);
+  const count = signal(Number(label.text));
 
   comp.connected(() => {
     const id = setInterval(() => {
-      count++;
-      label.native.textContent = count.toString();
+      count.set(count() + 1);
     }, 1_000);
 
     return () => {
       clearInterval(id);
     };
+  });
+
+  comp.effect(() => {
+    label.native.textContent = count().toString();
   });
 });
