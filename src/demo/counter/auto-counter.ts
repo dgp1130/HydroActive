c2e574e2efc692f6b7cdd76130331ba61a963456
Doug Parker <dgp1130@users.noreply.github.com>
2023-12-28 13:56:38 -0800

    Adds comments to `auto-counter` and `hello-world` examples.
    Should hopefully help explain how each of these components work.

--- src/demo/counter/auto-counter.ts
+++ src/demo/counter/auto-counter.ts
@@ -2,15 +2,30 @@ import { defineComponent } from 'hydroactive';
 
 /** Automatically increments the count over time. */
 export const AutoCounter = defineComponent('auto-counter', (comp) => {
+  // Create a "live" binding of the `<span>` element's text content, but
+  // interpreted as a `number`. Automatically parses the value.
   const count = comp.live('span', Number);
 
+  // This is the `hydrate` function, it is only called once per-component
+  // instance on hydration.
+
+  // Run some code when the component is connected to or disconnected from the
+  // document. This can be used to clean up resources which might cause the
+  // component to leak memory when not in use.
   comp.connected(() => {
-    const id = setInterval(() => {
+    // Executed when the component is connected to the DOM (or on hydration if
+    // already connected). Create a timer to automatically update the count
+    // every second.
+    const handle = setInterval(() => {
       count.set(count() + 1);
     }, 1_000);
 
+    // Executed when the component is disconnected from the DOM. Used to clean
+    // up resources created above.
     return () => {
-      clearInterval(id);
+      // Disable the timer so the component stops incrementing and allow the
+      // component to be garbage collected if no longer in use.
+      clearInterval(handle);
     };
   });
 });
--- src/demo/hello-world/hello-world.ts
+++ src/demo/hello-world/hello-world.ts
@@ -1,10 +1,12 @@
 import { defineComponent } from 'hydroactive';
+import { WriteableSignal } from 'hydroactive/signals.js';
 
 /** Says hello to HydroActive on hydration. */
 export const HelloWorld = defineComponent('hello-world', (comp) => {
-  // Create a "live" binding with the `#name` element and create a `string`
-  // signal bound to the element's `textContent`.
-  const name = comp.live('#name', String);
+  // Create a "live" binding with the `#name` element and receive a `string`
+  // signal bound to the element's `textContent`. Whenever the signal changes,
+  // the `#name` element will be automatically updated.
+  const name: WriteableSignal<string> = comp.live('#name', String);
 
   // Update the name in the DOM.
   name.set('HydroActive');
