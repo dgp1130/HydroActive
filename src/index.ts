51f22a7466380d4b9b125a11e264654b5ec13503
Doug Parker <dgp1130@users.noreply.github.com>
2024-03-02 18:59:51 -0800

    Exports new symbols from root `index.ts` barrel file.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+export { AttrAccessor } from './attribute-accessor.js';
 export { type HydrateLifecycle, defineComponent } from './component.js';
 export { ComponentRef, type OnDisconnect, type OnConnect } from './component-ref.js';
+export { Dehydrated } from './dehydrated.js';
+export { ElementAccessor } from './element-accessor.js';
 export { ElementRef } from './element-ref.js';
+export { type Queryable } from './queryable.js';
+export { hydrate, isHydrated } from './hydration.js';
