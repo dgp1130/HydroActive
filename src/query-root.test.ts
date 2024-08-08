1e421488960b06f685e248ad0e54d5b773c17c0a
Doug Parker <dgp1130@users.noreply.github.com>
2024-08-07 23:04:28 -0700

    WIP: Moves `query` and `queryAll` implementations into `QueryRoot`.
    Since `QueryRoot` has a concrete implementation which can be effectively composed, we no longer need these to be separate functions.

--- src/query-root.test.ts
+++ src/query-root.test.ts
@@ -10,5 +10,101 @@ describe('query-root', () => {
         expect(QueryRoot.from(el).root).toBe(el);
       });
     });
+
+    describe('query', () => {
+      it('returns the first queried element', () => {
+        const el = parseHtml(HTMLDivElement, `
+          <div>
+            <span>First</span>
+            <span>Second</span>
+          </div>
+        `);
+
+        expect(QueryRoot.from(el).query('span').unvalidatedElement.textContent)
+            .toBe('First');
+      });
+
+      it('throws when no element is found by default', () => {
+        const el = parseHtml(HTMLDivElement, `
+          <div>
+            <span>Hello, World!</span>
+          </div>
+        `);
+
+        expect(() => QueryRoot.from(el).query('input'))
+            .toThrowError(/did not resolve to an element/);
+      });
+
+      it('returns `null` when no element is found and explicitly `optional`', () => {
+        const el = parseHtml(HTMLDivElement, `
+          <div>
+            <span>Hello, World!</span>
+          </div>
+        `);
+
+        expect(QueryRoot.from(el).query('input', { optional: true }))
+            .toBeNull();
+      });
+
+      it('throws when no element is found and explicitly not `optional`', () => {
+        const el = parseHtml(HTMLDivElement, `
+          <div>
+            <span>Hello, World!</span>
+          </div>
+        `);
+
+        expect(() => QueryRoot.from(el).query('input', { optional: false }))
+            .toThrowError(/did not resolve to an element/);
+      });
+    });
+
+    describe('queryAll', () => {
+      it('returns the first queried element', () => {
+        const el = parseHtml(HTMLDivElement, `
+          <div>
+            <span>First</span>
+            <span>Second</span>
+            <span>Third</span>
+          </div>
+        `);
+
+        const items = QueryRoot.from(el).queryAll('span')
+            .map((span) => span.unvalidatedElement.textContent);
+        expect(items).toEqual([ 'First', 'Second', 'Third' ]);
+      });
+
+      it('throws when no element is found by default', () => {
+        const el = parseHtml(HTMLDivElement, `
+          <div>
+            <span>Hello, World!</span>
+          </div>
+        `);
+
+        expect(() => QueryRoot.from(el).queryAll('input'))
+            .toThrowError(/did not resolve to any elements/);
+      });
+
+      it('returns empty list when no element is found and explicitly `optional`', () => {
+        const el = parseHtml(HTMLDivElement, `
+          <div>
+            <span>Hello, World!</span>
+          </div>
+        `);
+
+        expect(QueryRoot.from(el).queryAll('input', { optional: true }))
+            .toEqual([]);
+      });
+
+      it('throws when no element is found and explicitly not `optional`', () => {
+        const el = parseHtml(HTMLDivElement, `
+          <div>
+            <span>Hello, World!</span>
+          </div>
+        `);
+
+        expect(() => QueryRoot.from(el).queryAll('input', { optional: false }))
+            .toThrowError(/did not resolve to any elements/);
+      });
+    });
   });
 });
