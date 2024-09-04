027fccd02f53775b714773311657704ca925851c
Doug Parker <dgp1130@users.noreply.github.com>
2024-09-03 21:57:29 -0700

    Deletes `ElementRef`.
    This has been obsoleted by `ElementAccessor` and is no longer used.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ export { type HydrateLifecycle, defineComponent } from './component.js';
 export { ComponentRef, type OnDisconnect, type OnConnect } from './component-ref.js';
 export { Dehydrated } from './dehydrated.js';
 export { ElementAccessor } from './element-accessor.js';
-export { ElementRef } from './element-ref.js';
 export { type Queryable } from './queryable.js';
 export { QueryRoot } from './query-root.js';
 export { hydrate, isHydrated } from './hydration.js';
