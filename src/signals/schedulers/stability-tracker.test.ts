efa052d088164b3029b3214ac3f26b34aaf4841b
Doug Parker <dgp1130@users.noreply.github.com>
2024-11-30 20:55:41 -0800

    Adds `StabilityTracker`.
    This is mostly a copy of the existing stability implementation in `Scheduler`, this new version will obsolete the one in `Scheduler`.
    
    The goal here is to decouple stability tracking from scheduling, allowing multiple `Scheduler` objects to be tracked as part of the same `.stable()` call. This allows one component to have a single `StabilityTracker` which tracks actions scheduled from many different `Scheduler` objects.

---
+++ src/signals/schedulers/stability-tracker.test.ts
@@ -0,0 +1,558 @@
+import { StabilityTracker } from './stability-tracker.js';
+import { Action, CancelAction, Scheduler } from './scheduler.js';
+
+/**
+ * Basic {@link Scheduler} implementation using `setTimeout` for testing. We can
+ * use Jasmine's mocked clock to trigger actions for testing purposes.
+ */
+class SetTimeoutScheduler extends Scheduler {
+  readonly #timeout: number;
+
+  public constructor(timeout: number = 0) {
+    super();
+
+    this.#timeout = timeout;
+  }
+
+  protected override scheduleAction(action: Action): CancelAction {
+    const handle = setTimeout(action, this.#timeout);
+    return () => { clearTimeout(handle); };
+  }
+}
+
+describe('stability-tracker', () => {
+  describe('StabilityTracker', () => {
+    beforeEach(() => { jasmine.clock().install(); });
+    afterEach(() => { jasmine.clock().uninstall(); });
+
+    describe('wrap', () => {
+      it('returns a scheduler which schedules actions and executes them', () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action = jasmine.createSpy<Action>('action');
+
+        scheduler.schedule(action);
+        expect(action).not.toHaveBeenCalled();
+
+        jasmine.clock().tick(0);
+
+        expect(action).toHaveBeenCalledOnceWith();
+      });
+
+      it('returns a scheduler which does not schedule an action multiple times', () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action = jasmine.createSpy<Action>('action');
+
+        scheduler.schedule(action);
+        expect(action).not.toHaveBeenCalled();
+
+        // Lots of time passes.
+        jasmine.clock().tick(1);
+        jasmine.clock().tick(2);
+        jasmine.clock().tick(3);
+
+        // Still only called once.
+        expect(action).toHaveBeenCalledOnceWith();
+      });
+
+      it('returns a scheduler which schedules multiple independent actions at the same time', () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action1 = jasmine.createSpy<Action>('action1');
+        const action2 = jasmine.createSpy<Action>('action2');
+
+        scheduler.schedule(action1);
+        scheduler.schedule(action2);
+        expect(action1).not.toHaveBeenCalled();
+        expect(action2).not.toHaveBeenCalled();
+
+        jasmine.clock().tick(0);
+
+        expect(action1).toHaveBeenCalledOnceWith();
+        expect(action2).toHaveBeenCalledOnceWith();
+      });
+
+      it('returns a scheduler which schedules multiple independent actions sequentially', () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action1 = jasmine.createSpy<Action>('action1');
+        const action2 = jasmine.createSpy<Action>('action2');
+
+        scheduler.schedule(action1);
+        expect(action1).not.toHaveBeenCalled();
+
+        jasmine.clock().tick(0);
+        expect(action1).toHaveBeenCalledOnceWith();
+        action1.calls.reset();
+
+        scheduler.schedule(action2);
+        expect(action2).not.toHaveBeenCalled();
+
+        jasmine.clock().tick(0);
+        expect(action1).not.toHaveBeenCalled();
+        expect(action2).toHaveBeenCalledOnceWith();
+      });
+
+      it('returns a scheduler which ignores errors from an action', async () => {
+        // Jasmine clock is very sensitive to errors and fails the test if a
+        // `setTimeout` callback throws. Need a real clock for this test.
+        jasmine.clock().uninstall();
+
+        try {
+          await jasmine.spyOnGlobalErrorsAsync(async (globalErrorSpy) => {
+            const tracker = StabilityTracker.from();
+            const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+            // WTR will log this uncaught error to the console, even though it
+            // is expected to be uncaught. Just give it a clear name and drop
+            // the stack to make it less noisy.
+            const err = new Error('Expected this error to be uncaught.');
+            delete err.stack;
+
+            const action1 = jasmine.createSpy<Action>('action1')
+                .and.throwError(err);
+
+            scheduler.schedule(action1);
+
+            await new Promise<void>((resolve) => {
+              setTimeout(() => { resolve(); });
+            });
+            expect(action1).toHaveBeenCalledOnceWith();
+
+            // Assert `setTimeout` callback throws, essentially an uncaught
+            // error.
+            expect(globalErrorSpy).toHaveBeenCalledOnceWith(err);
+
+            const action2 = jasmine.createSpy<Action>('action2');
+            scheduler.schedule(action2);
+            expect(action2).not.toHaveBeenCalled();
+
+            await new Promise<void>((resolve) => {
+              setTimeout(() => { resolve(); });
+            });
+            expect(action2).toHaveBeenCalledOnceWith();
+          });
+        } finally {
+          jasmine.clock().install(); // Re-install the clock for other tests.
+        }
+      });
+
+      it('returns a scheduler which cancels an action when the cancel callback is invoked', () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action = jasmine.createSpy<Action>('action');
+
+        const cancel = scheduler.schedule(action);
+        cancel();
+        expect(action).not.toHaveBeenCalled();
+
+        jasmine.clock().tick(0);
+        expect(action).not.toHaveBeenCalled();
+      });
+
+      it('returns a scheduler which cancels actions independently', () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action1 = jasmine.createSpy<Action>('action1');
+        const action2 = jasmine.createSpy<Action>('action2');
+
+        const cancel1 = scheduler.schedule(action1);
+        scheduler.schedule(action2);
+
+        cancel1();
+        expect(action1).not.toHaveBeenCalled();
+        expect(action2).not.toHaveBeenCalled();
+
+        jasmine.clock().tick(0);
+
+        expect(action1).not.toHaveBeenCalled();
+        expect(action2).toHaveBeenCalledOnceWith();
+      });
+
+      it('returns a scheduler which ignores canceling actions which have already executed', () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+        const action1 = jasmine.createSpy<Action>('action1');
+        const action2 = jasmine.createSpy<Action>('action2');
+
+        const cancel = scheduler.schedule(action1);
+
+        jasmine.clock().tick(0);
+        expect(action1).toHaveBeenCalledOnceWith();
+        action1.calls.reset();
+
+        expect(() => cancel()).not.toThrow();
+        expect(action1).not.toHaveBeenCalled();
+
+        scheduler.schedule(action2);
+
+        jasmine.clock().tick(0);
+        expect(action1).not.toHaveBeenCalledOnceWith();
+        expect(action2).toHaveBeenCalledOnceWith();
+      });
+
+      it('returns a scheduler which ignores canceling actions which have already been canceled', () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+        const action1 = jasmine.createSpy<Action>('action1');
+        const action2 = jasmine.createSpy<Action>('action2');
+
+        const cancel = scheduler.schedule(action1);
+
+        cancel(); // Cancel the action.
+        expect(() => cancel()).not.toThrow(); // Extra `cancel` call.
+        expect(action1).not.toHaveBeenCalled();
+
+        scheduler.schedule(action2);
+
+        jasmine.clock().tick(0);
+        expect(action1).not.toHaveBeenCalledOnceWith();
+        expect(action2).toHaveBeenCalledOnceWith();
+      });
+
+      it('returns a scheduler which throws if the underlying `scheduleAction` throws', () => {
+        const err = new Error('Oh noes!');
+        class BadScheduler extends Scheduler {
+          protected override scheduleAction(): never {
+            throw err;
+          }
+        }
+
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new BadScheduler());
+
+        expect(() => scheduler.schedule(() => {})).toThrow(err);
+      });
+
+      it('ignores actions scheduled on the input `Scheduler`', () => {
+        const tracker = StabilityTracker.from();
+        const inputScheduler = new SetTimeoutScheduler();
+        tracker.wrap(inputScheduler);
+
+        inputScheduler.schedule(() => {});
+        expect(tracker.isStable()).toBeTrue(); // Ignores `inputScheduler`.
+
+        jasmine.clock().tick(0);
+      });
+    });
+
+    describe('isStable', () => {
+      it('returns true before any actions are scheduled', () => {
+        const tracker = StabilityTracker.from();
+
+        expect(tracker.isStable()).toBeTrue();
+      });
+
+      it('returns whether the scheduler is stable', () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        scheduler.schedule(() => {});
+        expect(tracker.isStable()).toBeFalse();
+
+        jasmine.clock().tick(0);
+        expect(tracker.isStable()).toBeTrue();
+      });
+
+      it('manages stability across multiple schedulers', () => {
+        const tracker = StabilityTracker.from();
+        const scheduler1 = tracker.wrap(new SetTimeoutScheduler());
+        const scheduler2 = tracker.wrap(new SetTimeoutScheduler(1));
+
+        const action1 = jasmine.createSpy<Action>('action1');
+        scheduler1.schedule(action1);
+        const action2 = jasmine.createSpy<Action>('action2');
+        scheduler2.schedule(action2);
+
+        expect(tracker.isStable()).toBeFalse();
+
+        jasmine.clock().tick(0);
+        expect(action1).toHaveBeenCalled();
+        expect(action2).not.toHaveBeenCalled();
+        expect(tracker.isStable()).toBeFalse(); // Still waiting for `action2`.
+
+        jasmine.clock().tick(1);
+        expect(action2).toHaveBeenCalled();
+        expect(tracker.isStable()).toBeTrue();
+      });
+
+      it('returns true when stable after a canceled action', () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const cancel = scheduler.schedule(() => {});
+        expect(tracker.isStable()).toBeFalse();
+
+        cancel();
+        expect(tracker.isStable()).toBeTrue();
+      });
+
+      it('returns false when unstable after a canceled action', () => {
+        const tracker = StabilityTracker.from();
+        const scheduler1 = tracker.wrap(new SetTimeoutScheduler());
+        const scheduler2 = tracker.wrap(new SetTimeoutScheduler());
+
+        const action1 = jasmine.createSpy<Action>('action1');
+        const cancel = scheduler1.schedule(action1);
+
+        const action2 = jasmine.createSpy<Action>('action2');
+        scheduler2.schedule(action2);
+
+        expect(tracker.isStable()).toBeFalse();
+
+        cancel();
+        expect(tracker.isStable()).toBeFalse();
+
+        jasmine.clock().tick(0);
+        expect(action1).not.toHaveBeenCalled();
+        expect(action2).toHaveBeenCalled();
+        expect(tracker.isStable()).toBeTrue();
+      });
+
+      it('handles actions which are both executed *and* canceled', async () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action = jasmine.createSpy<Action>('action');
+
+        const cancel = scheduler.schedule(() => {});
+        expect(tracker.isStable()).toBeFalse();
+
+        jasmine.clock().tick(0); // Executes action.
+        expect(tracker.isStable()).toBeTrue();
+
+        cancel(); // Canceled after execution.
+        expect(tracker.isStable()).toBeTrue();
+
+        // Stability should not be corrupted.
+        scheduler.schedule(action);
+        expect(tracker.isStable()).toBeFalse();
+
+        jasmine.clock().tick(0);
+        expect(tracker.isStable()).toBeTrue();
+        expect(action).toHaveBeenCalled();
+      });
+
+      it('handles actions which are canceled multiple times', async () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action = jasmine.createSpy<Action>('action');
+
+        const cancel = scheduler.schedule(() => {});
+        expect(tracker.isStable()).toBeFalse();
+
+        // *Incorrectly* cancel the action twice.
+        cancel();
+        expect(tracker.isStable()).toBeTrue();
+
+        cancel();
+        expect(tracker.isStable()).toBeTrue();
+
+        // Stability should not be corrupted.
+        scheduler.schedule(action);
+        expect(tracker.isStable()).toBeFalse();
+        jasmine.clock().tick(0);
+        expect(tracker.isStable()).toBeTrue();
+        expect(action).toHaveBeenCalled();
+      });
+    });
+
+    describe('stable', () => {
+      it('resolves immediately when no actions are pending', async () => {
+        const tracker = StabilityTracker.from();
+
+        await expectAsync(tracker.stable()).toBeResolved();
+      });
+
+      it('waits and resolves once pending actions have executed', async () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        scheduler.schedule(() => {});
+
+        const stable = tracker.stable();
+        await expectAsync(stable).toBePending(); // Should be initially pending.
+
+        jasmine.clock().tick(0);
+        await expectAsync(stable).toBeResolved();
+      });
+
+      it('waits and resolves once pending actions are canceled', async () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action = jasmine.createSpy<Action>('action');
+
+        const cancel = scheduler.schedule(action);
+
+        const stable = tracker.stable();
+        await expectAsync(stable).toBePending(); // Should be initially pending.
+
+        cancel();
+        await expectAsync(stable).toBeResolved();
+        expect(action).not.toHaveBeenCalled();
+      });
+
+      it('waits and resolves once all pending actions have executed across multiple schedulers', async () => {
+        const tracker = StabilityTracker.from();
+        const scheduler1 = tracker.wrap(new SetTimeoutScheduler());
+        const scheduler2 = tracker.wrap(new SetTimeoutScheduler(1));
+
+        const action1 = jasmine.createSpy<Action>('action1');
+        scheduler1.schedule(action1);
+        const action2 = jasmine.createSpy<Action>('action2');
+        scheduler2.schedule(action2);
+
+        const stable = tracker.stable();
+        await expectAsync(stable).toBePending(); // Should be initially pending.
+
+        jasmine.clock().tick(0);
+        expect(action1).toHaveBeenCalled();
+        expect(action2).not.toHaveBeenCalled();
+        await expectAsync(stable).toBePending(); // Still waiting for `action2`.
+
+        jasmine.clock().tick(1);
+        expect(action2).toHaveBeenCalled();
+        await expectAsync(stable).toBeResolved();
+      });
+
+      it('waits for actions scheduled in an action callback', async () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action = jasmine.createSpy<Action>('action');
+
+        scheduler.schedule(() => {
+          scheduler.schedule(action);
+        });
+
+        await pollAndTrigger(tracker.stable(), () => {
+          jasmine.clock().tick(2);
+        });
+        expect(action).toHaveBeenCalled();
+      });
+
+      // If this last action queues a microtask which schedules a new action,
+      // then that microtask would be executed *after* the scheduler decides it
+      // is stable but before `await tracker.stable()` can resume, meaning the
+      // scheduler might still be unstable even after the `await`. This test
+      // exercises this exact case.
+      it('waits for actions scheduled in microtasks from an action', async () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action = jasmine.createSpy<Action>('action');
+
+        scheduler.schedule(() => {
+          queueMicrotask(() => {
+            scheduler.schedule(action);
+          });
+        });
+
+        await pollAndTrigger(tracker.stable(), async () => {
+          jasmine.clock().tick(0);
+          await microTask();
+          jasmine.clock().tick(0);
+        });
+        expect(action).toHaveBeenCalled();
+      });
+
+      // Replicate the previous test case multiple times in a row to verify that
+      // the scheduler correctly waits each time it happens consecutively.
+      it('continuously waits for actions scheduled in microtasks from an action', async () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action = jasmine.createSpy<Action>('action');
+
+        scheduler.schedule(() => {
+          queueMicrotask(() => {
+            scheduler.schedule(() => {
+              queueMicrotask(() => {
+                scheduler.schedule(action);
+              });
+            });
+          });
+        });
+
+        await pollAndTrigger(tracker.stable(), async () => {
+          jasmine.clock().tick(0);
+          await microTask();
+          jasmine.clock().tick(0);
+          await microTask();
+          jasmine.clock().tick(0);
+        });
+        expect(action).toHaveBeenCalled();
+      });
+
+      it('handles actions which are both executed *and* canceled', async () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action = jasmine.createSpy<Action>('action');
+
+        const cancel = scheduler.schedule(() => {});
+        await pollAndTrigger(tracker.stable(), () => {
+          jasmine.clock().tick(0);
+        });
+        cancel(); // Canceled after execution.
+
+        scheduler.schedule(action);
+
+        // Stability should not be corrupted.
+        jasmine.clock().tick(0);
+        await expectAsync(tracker.stable()).toBeResolved();
+        expect(action).toHaveBeenCalled();
+      });
+
+      it('handles actions which are canceled multiple times', async () => {
+        const tracker = StabilityTracker.from();
+        const scheduler = tracker.wrap(new SetTimeoutScheduler());
+
+        const action = jasmine.createSpy<Action>('action');
+
+        // Schedule an action and *incorrectly* cancel it twice.
+        const cancel = scheduler.schedule(() => {});
+        cancel();
+        cancel();
+
+        // Stability should not be corrupted.
+        scheduler.schedule(action);
+        await pollAndTrigger(tracker.stable(), () => {
+          jasmine.clock().tick(0);
+        });
+        expect(action).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+/**
+ * Syntax sugar for taking an existing `Promise`, running an action, then
+ * `await`-ing that `Promise`. Allows convenient use of `await` while ensuring
+ * the `Promise` is created *before* the action occurs.
+ */
+function pollAndTrigger<Value>(
+  promise: Promise<Value>,
+  callback: () => void,
+): Promise<Value> {
+  callback();
+  return promise;
+}
+
+/** Waits one microtask. */
+function microTask(): Promise<void> {
+  // `resolve` naturally waits one microtask before triggering
+  // `Promise.prototype.then`.
+  return new Promise<void>((resolve) => {
+    resolve();
+  });
+}
---
+++ src/signals/schedulers/stability-tracker.ts
@@ -0,0 +1,189 @@
+import { Action, CancelAction, Scheduler } from './scheduler.js';
+
+/**
+ * Tracks stability based on whether any pending actions are waiting to be
+ * executed from any wrapped {@link Scheduler} objects.
+ *
+ * Provides a {@link StabilityTracker.prototype.wrap} function which wraps a
+ * {@link Scheduler} into a new one which is tracked by the wrapping
+ * {@link StabilityTracker}. Reports stability based on any pending actions.
+ */
+export class StabilityTracker {
+  private constructor() {}
+
+  /**
+   * How many actions are "pending" (scheduled and not yet executed or
+   * canceled.)
+   */
+  #pendingActions = 0;
+  private get pendingActions(): number {
+    return this.#pendingActions;
+  }
+  private set pendingActions(value: number) {
+    this.#pendingActions = value;
+
+    // If the scheduler just became stable, resolve any open promises waiting
+    // for the scheduler to become stable.
+    if (this.isStable()) this.#resolveStablePromise?.();
+  }
+
+  /**
+   * A {@link Promise} which will resolve when the scheduler becomes "stable"
+   * (no currently scheduled actions).
+   */
+  #stablePromise?: Promise<void>;
+
+  /** The `resolve` function of `#stablePromise`. */
+  #resolveStablePromise?: () => void;
+
+  /** Provides an {@link StabilityTracker}. */
+  public static from(): StabilityTracker {
+    return new StabilityTracker();
+  }
+
+  /**
+   * Wraps the given {@link Scheduler} by creating a new {@link Scheduler} tied
+   * to this {@link StabilityTracker}. When the resulting {@link Scheduler}
+   * schedules an action, this {@link StabilityTracker} tracks its state and
+   * reports stability.
+   *
+   * Example:
+   *
+   * ```typescript
+   * const tracker = StabilityTracker.from();
+   * const scheduler = tracker.wrap(new SomeScheduler());
+   *
+   * tracker.isStable(); // `true`, nothing scheduled.
+   *
+   * let executed = false;
+   * scheduler.schedule(() => {
+   *   executed = true;
+   * });
+   * tracker.isStable(); // `false`, something scheduled.
+   *
+   * await tracker.stable();
+   * executed; // `true`, waited for action.
+   * ```
+   */
+  public wrap(scheduler: Scheduler): Scheduler {
+    return WrappedScheduler.from(scheduler, (schedule) => {
+      return (action) => {
+        let finalized = false;
+        const finalize = (): void => {
+          // Only finalize an action once.
+          if (finalized) return;
+          finalized = true;
+
+          // Action is no longer pending, check if we need to resolve any open
+          // stability promises.
+          this.pendingActions--;
+        };
+
+        this.pendingActions++;
+        const cancel = schedule(() => {
+          try {
+            action();
+          } finally {
+            finalize();
+          }
+        });
+
+        return () => {
+          cancel();
+          finalize();
+        };
+      };
+    });
+  }
+
+  /**
+   * Returns whether or not this {@link Scheduler} is stable. A
+   * {@link Scheduler} is considered "stable" if there are no currently
+   * scheduled actions which have not yet been executed or canceled.
+   *
+   * @returns Whether or not the scheduler is stable.
+   */
+  public isStable(): boolean {
+    return this.#pendingActions === 0;
+  }
+
+  /**
+   * Returns a {@link Promise} which resolves when the scheduler becomes stable.
+   * A {@link Scheduler} is considered "stable" if there are no currently
+   * scheduled actions which have not yet been executed or canceled.
+   *
+   * @returns A {@link Promise} which resolves when the scheduler becomes
+   *     stable.
+   */
+  public async stable(): Promise<void> {
+    // Check if the scheduler is already stable.
+    if (this.isStable()) return;
+
+    // Create a new `Promise` to track stability if one doesn't already exist.
+    this.#stablePromise ??= new Promise<void>((resolve) => {
+      this.#resolveStablePromise = () => {
+        resolve();
+        this.#stablePromise = undefined;
+        this.#resolveStablePromise = undefined;
+      };
+    });
+
+    // Wait until the schedule is stable.
+    await this.#stablePromise;
+
+    // If the last action executed happens to queue a microtask, then that
+    // microtask will execute *after* `this.stablePromise` is resolved, but
+    // before `await this.stablePromise` resumes. Normally that's fine, but if
+    // the microtask also happens to *schedule* a new action on this scheduler,
+    // we may end up resolving a `scheduler.stable()` promise when the scheduler
+    // has already become unstable again.
+    //
+    // To address this, we double check stability to be sure and recursively
+    // re-await if a new action was added.
+    if (!this.isStable()) await this.stable();
+  }
+}
+
+/** Schedule an action and return a function to cancel it. */
+type Schedule = (action: Action) => CancelAction;
+
+/** Transform a {@link Schedule} function into another implementation. */
+type ScheduleTransform = (schedule: Schedule) => Schedule;
+
+/**
+ * A {@link Scheduler} which wraps another {@link Scheduler} by proxying the
+ * {@link Scheduler.prototype.schedule} function.
+ */
+class WrappedScheduler extends Scheduler {
+  readonly #schedule: Schedule;
+
+  private constructor(schedule: Schedule) {
+    super();
+
+    this.#schedule = schedule;
+  }
+
+  /**
+   * Returns a {@link WrappedScheduler} which wraps the provided
+   * {@link Scheduler} using {@link transformSchedule}. Does not modify
+   * {@link wrapped}.
+   *
+   * @param wrapped The {@link Scheduler} to wrap.
+   * @param transformSchedule A {@link ScheduleTransform} which transforms the
+   *     {@link Scheduler.prototype.schedule} function of {@link wrapped}.
+   * @returns A {@link WrappedScheduler} which wraps the input
+   *     {@link Scheduler}.
+   */
+  public static from(
+    wrapped: Scheduler,
+    transformSchedule: ScheduleTransform,
+  ): WrappedScheduler {
+    // Transform the `schedule` method of the input `Scheduler`.
+    const schedule = transformSchedule(wrapped.schedule.bind(wrapped));
+    return new WrappedScheduler(schedule);
+  }
+
+  public override scheduleAction(action: Action): CancelAction {
+    return this.#schedule(action);
+  }
+}
